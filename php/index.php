67132298936669cf0ce9d7cf8e75e384655c04bd
Hayato <hayato.314.nico@gmail.com>
2023-01-03 03:38:38 +0900

    DiaperAdventure 初コミット

---
+++ php/index.php
@@ -0,0 +1,39 @@
+<?php
+
+// 各種項目設定
+$dbn ='mysql:dbname=Diaper;charset=utf8mb4;port=3306;host=localhost';
+$user = 'root';
+$pwd = '';
+
+// DB接続
+try {
+  $pdo = new PDO($dbn, $user, $pwd);
+} catch (PDOException $e) {
+  echo json_encode(["db error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+// 「dbError:...」が表示されたらdb接続でエラーが発生していることがわかる．
+
+// SQL作成&実行
+$sql = 'INSERT INTO Diaper (DiaperID, diaperName, absorbency, favoriteFlag) VALUES (NULL, :todo, :deadline, now(), now())';
+
+$stmt = $pdo->prepare($sql);
+
+// バインド変数を設定
+$stmt->bindValue(':todo', $todo, PDO::PARAM_STR);
+$stmt->bindValue(':deadline', $deadline, PDO::PARAM_STR);
+
+// SQL実行（実行に失敗すると `sql error ...` が出力される）
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+
+
+
+
+?>
---
+++ readme.md
@@ -0,0 +1,9 @@
+HTMLファイルでPHPを使って共通部分を管理する  
+https://kigiroku.com/frontend/html-php.html  
+
+PHP include  
+https://www.php.net/manual/ja/function.include.php  
+
+
+おむつ  
+https://osusume.mynavi.jp/articles/5881/
